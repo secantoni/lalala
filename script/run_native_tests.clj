56b7e7017fc53de2b11b3fa9ae0e91d0182bf4bd
secantoni <secundinus.antoni@gmail.com>
2017-03-01 13:26:42 +0100

    .travis.yml

--- script/run_native_tests.clj
+++ script/run_native_tests.clj
@@ -1,10 +1,16 @@
 
 (require 'lalala.client.tests-receiver)
 (require 'clojure.java.shell)
+(require '[clojure.core.async :as a])
 
 (println "Starting Tests Receiver...")
 (lalala.client.tests-receiver/run)
 (println "Tests Receiver Running...")
+
+(a/go
+  (clojure.java.shell/sh "./node_modules/.bin/react-native" "start"))
+
+(a/<!! (a/timeout 3000))
 
 (println "Starting Tests on Simulator...")
 (let [r (clojure.java.shell/sh "./node_modules/.bin/react-native" "run-ios")]
